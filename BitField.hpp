c6b2433e596afd0c6d262f6e32b7cf499a5e7f5e
AlKurpiakov <s23380350@unn.ru>
2024-10-03 23:46:23 +0300

    12

--- BitField.hpp
+++ BitField.hpp
@@ -4,6 +4,11 @@
 #include <unordered_map>
 #include <algorithm>
 #include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#pragma once
 using namespace std;
 
 
@@ -14,6 +19,7 @@ private:
     uint8_t* _mem;
     size_t GetMemIndex(size_t n);
     uint8_t GetMask(size_t n);
+
 public:
     BitField(size_t sizeBit);
     ~BitField();
@@ -29,5 +35,43 @@ public:
     BitField& operator&(const BitField& tmp) const;
     BitField& operator|(const BitField& tmp) const;
     void operator~();
+
+    friend istream &operator>>(istream &in, BitField &x) {
+        size_t sizeBit;
+        in >> sizeBit;
+        x._sizeBit = sizeBit;
+        x._sizeMem = (x._sizeBit / 8) + 1;
+        x._mem = new uint8_t[x._sizeMem];
+        string tmp;
+        string close_one = " ", close_two = "\t";
+        while (1) {
+            in >> tmp;
+            if (tmp == close_one || tmp == close_two) {
+                break;
+            }
+            x.SetBit(stoi(tmp));
+        }
+        return in;
+    }
+
+
+
+    friend ostream &operator<<(ostream &os, const BitField &x) {
+        int flag = 0;
+        BitField tmp(x);
+        os << "{";
+        for (int j = 0; j < tmp._sizeMem; j++)
+            for (int i = 0; i < 8; i++) {
+                if ((tmp._mem[j] & 1) && (j == tmp._sizeMem - 1) && (i == 7)) {
+                    os << flag;
+                } else if (tmp._mem[j] & 1) {
+                    os << flag << " ,";
+                }
+                flag++;
+                tmp._mem[j] >> 1;
+            }
+        return os << "}";
+    };
+
 };
 
