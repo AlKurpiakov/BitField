c06254694ad50399fac997e4bad35068b22f961c
AlKurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2024-10-12 13:38:33 +0300

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -8,5 +8,5 @@ Set::Set(const BitField& tmp): _maxPower(tmp.GetLenght()), _bitfield(tmp){}
 
 Set::Set(const Set& tmp) { _maxPower = tmp._maxPower; _bitfield = tmp._bitfield; }
 
-Set::Set(const size_t maxPower){_maxPower=maxPower; _bitfield(maxPower)}
+Set::Set(const size_t maxPower):_bitfield(maxPower){_maxPower=maxPower;}
 
